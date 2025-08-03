46a32e03bf5e70ad86bea6ebe55b67962e8e1aa5
Alex Al Basosi <a.albasosi95@gmail.com>
2025-08-03 21:44:51 +0100

    Updated timeline with Education

--- src/components/career-timeline/CareerTimeline.ts
+++ src/components/career-timeline/CareerTimeline.ts
@@ -1,13 +1,41 @@
 export const careerTimelineContent = {
     title: 'Career & Education',
     description: 'A brief overview of my career journey.',
-    jobs: [
-        'Senior Software Engineer at Lloyds Banking Group',
-        'Technical Instructor at Cisco AppDynamics',
-        'Consultant at Cisco AppDynamics',
-        'Full Stack Developer at IBM CIO',
-        'Blockchain Developer at IBM Garage',
-        'Developer Advocate Intern at IBM Cloud'
+    positions: [
+        {
+            title: 'Senior Software Engineer at Lloyds Banking Group',
+            tag: 'career'
+        },
+        {
+            title: 'Started MSc in Software Engineering at University of Oxford',
+            tag: 'education',
+            status: 'in-progress'
+        },
+        {
+            title: 'Technical Instructor at Cisco AppDynamics',
+            tag: 'career'
+        },
+        {
+            title: 'Consultant at Cisco AppDynamics',
+            tag: 'career'
+        },
+        {
+            title: 'Full Stack Developer at IBM CIO',
+            tag: 'career'
+        },
+        {
+            title: 'Blockchain Developer at IBM Garage',
+            tag: 'career'
+        },
+        {
+            title: 'Developer Advocate Intern at IBM Cloud',
+            tag: 'career'
+        },
+        {
+            title: 'Completed BSc in Computer Science from the University of Wollongong',
+            tag: 'education',
+            status: 'complete'
+        },
     ],
     dotColors: [
         'primary',
--- src/components/career-timeline/CareerTimeline.tsx
+++ src/components/career-timeline/CareerTimeline.tsx
@@ -3,6 +3,7 @@ import {
 } from '@mui/lab';
 import { Container, Box, Typography } from '@mui/material';
 import { careerTimelineContent } from './CareerTimeline';
+import { School, SchoolOutlined } from '@mui/icons-material';
 
 export default function BasicTimeline() {
   return (
@@ -19,17 +20,23 @@ export default function BasicTimeline() {
                 <Typography>{careerTimelineContent.description}</Typography>
             </div>
             <Timeline position='alternate-reverse'>
-                {careerTimelineContent.jobs.map((job, index) => {
+                {careerTimelineContent.positions.map((position, index) => {
                     return (
                         <TimelineItem>
                             <TimelineSeparator>
-                            <TimelineDot 
-                                variant='outlined'
-                                color={index % 3 === 0 ? 'success' : index % 3 === 1 ? 'secondary' : 'primary'}
-                            />
+                            {
+                                position.tag == 'career' ? 
+                                    <TimelineDot 
+                                        variant='outlined'
+                                        color={index% 3 === 0 ? 'success' : index % 3 === 1 ? 'secondary' : 'primary'}
+                                    /> : 
+                                        <TimelineDot>
+                                            {position.status === 'complete' ? <School /> : <SchoolOutlined />}
+                                        </TimelineDot>
+                            }
                             <TimelineConnector />
                             </TimelineSeparator>
-                            <TimelineContent>{job}</TimelineContent>
+                            <TimelineContent>{position.title}</TimelineContent>
                         </TimelineItem>
                     )
                 })}
