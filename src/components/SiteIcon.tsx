352cca160c65b772268a8574b16cc6f80c25bbb4
Alex Randall <a.albasosi95@gmail.com>
2025-08-10 19:45:36 +0100

    Added SiteIcon navigation

--- src/components/SiteIcon.tsx
+++ src/components/SiteIcon.tsx
@@ -1,6 +1,8 @@
 import { Box } from "@mui/material";
+import { useNavigate } from "react-router-dom";
 
 export default function SiteIcon({ icon = 'AR2' }: { icon?: string }) {
+  const navigate = useNavigate();
   const iconPath = `${import.meta.env.BASE_URL}assets/images/${icon}.png`;
 
   return (
@@ -8,12 +10,14 @@ export default function SiteIcon({ icon = 'AR2' }: { icon?: string }) {
       component="img"
       src={iconPath}
       alt={`${icon} icon`}
+      onClick={() => navigate('/')}
       sx={{
         width: '100px', // Adjust width as needed
         height: 'auto', // Maintain aspect ratio
         marginRight: '8px', // Add spacing if needed
         justifySelf: 'center',
-        pb: { xs: 0, md: 0.5}
+        pb: { xs: 0, md: 0.5},
+        cursor: 'pointer',
       }}
     />
   );
