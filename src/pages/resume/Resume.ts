2bec95bff3b9275ab2a2a5f8380cccdef2189e0f
Alex Randall <a.albasosi95@gmail.com>
2025-11-12 22:49:59 +0000

    Fixed typo on Resume page

--- src/pages/resume/Resume.ts
+++ src/pages/resume/Resume.ts
@@ -1,6 +1,6 @@
 export const resumeContent = {
     title: 'Resume',
-    description: 'This page serves as a dynamic overciew of my professional journey, key skills, and experience. For a comprehensive version, please feel free to download my CV below.',
+    description: 'This page serves as a dynamic overview of my professional journey, key skills, and experience. For a comprehensive version, please feel free to download my CV below.',
     tabs: ['Skills', 'Experience', 'Certifications'],
     comingSoonText: 'Content coming soon!',
 }
